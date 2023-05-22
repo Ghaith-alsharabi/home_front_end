88beaa4d6161bf864b58554cbed4e834b867fdd6
Ghaith <ghais_93@hotmail.com>
2023-05-22 02:30:02 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 function generateAndRenderHTML() {
   const htmlCode = `
+  <body>
     <p>hi hi</p>
+    </body>
 `;
   const bodyElement = document.querySelector('body');
   bodyElement.innerHTML = htmlCode;
