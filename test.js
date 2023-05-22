36cc7ff566a99cf56a1c874c21c09824c1566112
Ghaith <ghais_93@hotmail.com>
2023-05-22 02:34:50 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function generateAndRenderHTML() {
   const htmlCode = `
   <body>
     <p>hi hi</p>
-    </body>
+  </body>
 `;
   const bodyElement = document.querySelector('body');
   bodyElement.innerHTML = htmlCode;
