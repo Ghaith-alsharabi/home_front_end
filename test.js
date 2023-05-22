bcf50103c8380084c61826921970440e2b9f4e81
Ghaith <ghais_93@hotmail.com>
2023-05-22 03:51:05 +0200

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-  fetch('https://raw.githubusercontent.com/Ghaith-alsharabi/home_front_end/master/test.html')
-    .then(response => response.text())
-    .then(htmlContent => {
-      const templateContainer = document.getElementById('templateContainer');
-      templateContainer.innerHTML = htmlContent;
-    })
-    .catch(error => console.error('Error fetching HTML template:', error))
