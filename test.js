c00a1290db28bb29f4acab52b2e364764b089a62
Ghaith <ghais_93@hotmail.com>
2023-05-22 03:29:44 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,5 @@
-function generateHTMLCode() {
-  const htmlCode = `<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-    <script src="https://raw.githubusercontent.com/your-username/your-repo/your-branch/your-script.js"></script>
-</body>
-</html>`;
-
-  return htmlCode;
+function renderHello() {
+  var element = document.createElement("div");
+  element.innerHTML = "Hello";
+  document.body.appendChild(element);
 }
