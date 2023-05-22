b05ebd58fc6df5d5d241f3107c3e86c2448b3f57
Ghaith-alsharabi <ghais_93@hotmail.com>
2023-05-22 02:18:37 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,16 @@
+function generateAndRenderHTML() {
+  const htmlCode = `<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>title</title>
+</head>
+<body>
+    
+</body>
+</html>`;
+
+  document.write(htmlCode);
+}
+
+generateAndRenderHTML();
