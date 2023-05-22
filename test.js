ecf6b1645d1372cdaf08e2b4e99ec3480a449bc6
Ghaith <ghais_93@hotmail.com>
2023-05-22 03:47:21 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-  var element = document.createElement("div");
-  element.innerHTML = "Hello";
-  document.body.appendChild(element);
+  fetch('https://raw.githubusercontent.com/Ghaith-alsharabi/home_front_end/master/test.html')
+    .then(response => response.text())
+    .then(htmlContent => {
+      const templateContainer = document.getElementById('templateContainer');
+      templateContainer.innerHTML = htmlContent;
+    })
+    .catch(error => console.error('Error fetching HTML template:', error))
